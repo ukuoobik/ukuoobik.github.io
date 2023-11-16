24f0a8d2dfbccaa823f62792b9be04ffc8150ae4
ukuoobik <113310136+ukuoobik@users.noreply.github.com>
2023-11-16 22:27:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const flashcardlink2 = document.querySelector('#flashcardlink2');
+
+flashcardlink2.addEventListener('mouseleave', () => {
+  flashcardlink2.style.animationPlayState = 'running';
+});
