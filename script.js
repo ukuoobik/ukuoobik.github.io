cfe90cf50a30585f8dbb1ec6d779b99fd39f739f
ukuoobik <113310136+ukuoobik@users.noreply.github.com>
2023-12-14 00:55:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const flashcardlink2 = document.querySelector('#flashcardlink2');
-
-flashcardlink2.addEventListener('mouseleave', () => {
-  flashcardlink2.style.animationPlayState = 'running';
-});
